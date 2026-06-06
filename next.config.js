54669ee195d4f912f640de2d049634daf5c410d3
Willy Willy <tummywilly48@gmail.com>
2026-06-06 07:40:18 +0300

    Fix Vercel production build settings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
 	serverExternalPackages: ["better-sqlite3", "@prisma/adapter-better-sqlite3"],
+	turbopack: {
+		root: path.join(__dirname),
+	},
 };
 
 module.exports = nextConfig;
